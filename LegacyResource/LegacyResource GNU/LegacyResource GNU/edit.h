17cb86f7b46e4c9cce88e21981bcb74c7cc75212
Snow32 <droidspeech@gmail.com>
2025-12-19 00:19:38 +0300

    clearingg

--- LegacyResource/LegacyResource GNU/LegacyResource GNU/edit.h
+++
@@ -1,9 +0,0 @@
-#ifndef EDIT_H
-#define EDIT_H
-
-#include <windows.h>
-
-// Function to create a window with a button and block until the window is closed
-bool CreateEditorWindow(HINSTANCE hInstance);
-
-#endif // STARTUP_H
--- LegacyResource/LegacyResource GNU/LegacyResource GNU/window.h
+++
@@ -1,8 +0,0 @@
-#ifndef STARTUPWIN_H
-#define STARTUPWIN_H
-
-#include <windows.h>
-
-HWND startupwin(HINSTANCE hInstance, WNDPROC WindowProc);
-bool CALLBACK SetFont(HWND child, LPARAM font);
-#endif // STARTUPWIN_H#pragma once
